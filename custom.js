308e2beb50991d48a7fd9fdddbc31e66ef3ec41d
Halit IZGIN <halit_izgin@outlook.com.tr>
2019-06-07 15:56:32 +0300

    AngularJS Ders 6 - ng-if, ng-class, ng-style

--- custom.js
+++ custom.js
@@ -1,4 +1,12 @@
 var app = angular.module('App', []);
 app.controller('Controller', $scope => {
-    
+    $scope.style = 'siyah';
+    $scope.style1 = {
+        'background-color': 'black', 
+        'color': 'white'
+    }
+    $scope.style2 = {
+        'background-color': 'gray', 
+        'color': 'blue'
+    }
 });
